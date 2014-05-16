73764439be09aa171edd0936e38af8cc41b48dee
Ilya S <public.ilya.s@gmail.com>
2014-05-16 18:00:42 +0700

    generate ParserSpec

---
+++ source/Exo/Parser.php
@@ -0,0 +1,7 @@
+<?php namespace Exo;
+
+class Parser {
+
+
+
+}
---
+++ tests/Spec/Exo/ParserSpec.php
@@ -0,0 +1,12 @@
+<?php namespace Spec\Exo;
+
+use PhpSpec\ObjectBehavior;
+
+class ParserSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Exo\Parser');
+    }
+
+}
