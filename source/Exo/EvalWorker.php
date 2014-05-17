bf0db18e3094282436f15317a336a4766bccadef
Ilya S <public.ilya.s@gmail.com>
2014-05-17 17:24:01 +0700

    create EvalWorker

---
+++ source/Exo/EvalWorker.php
@@ -0,0 +1,17 @@
+<?php namespace Exo;
+
+class EvalWorker {
+
+    /**
+     * Evaluate given $code.
+     *
+     * @param string $code
+     * @return mixed
+     */
+    public function evaluate($code)
+    {
+
+    }
+
+}
+
---
+++ tests/Spec/Exo/EvalWorkerSpec.php
@@ -0,0 +1,13 @@
+<?php namespace Spec\Exo;
+
+use PhpSpec\ObjectBehavior;
+
+class EvalWorkerSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Exo\EvalWorker');
+    }
+
+}
+
