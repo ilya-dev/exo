a2d786fb0f04ef55b78281fceabca8d01f445064
Ilya S <public.ilya.s@gmail.com>
2014-05-17 19:39:07 +0700

    work on proper class names

---
+++ source/Exo/Evaluator.php
@@ -0,0 +1,47 @@
+<?php namespace Exo;
+
+class Evaluator {
+
+    /**
+     * Evaluate given $code.
+     *
+     * @param string $code
+     * @return mixed
+     */
+    public function evaluate($code)
+    {
+        return $this->toString(eval ("return {$code};"));
+    }
+
+    /**
+     * Convert given $value to a string.
+     *
+     * @param mixed $value
+     * @return string
+     */
+    protected function toString($value)
+    {
+        switch (gettype($value))
+        {
+            case 'object':
+                return $this->objectToString($value);
+            default:
+                return var_export($value, true);
+        }
+    }
+
+    /**
+     * Convert given $object to a string.
+     *
+     * @param mixed $object
+     * @return string
+     */
+    protected function objectToString($object)
+    {
+        return sprintf(
+            "[object:%s:%s]", get_class($object), spl_object_hash($object)
+        );
+    }
+
+}
+
