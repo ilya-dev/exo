f5e83a49805b2006f7d110a7d38ced088b17a108
Ilya S <public.ilya.s@gmail.com>
2014-05-18 00:26:52 +0700

    inject dependencies

--- source/Exo/Builder.php
+++ source/Exo/Builder.php
@@ -3,6 +3,34 @@
 class Builder {
 
     /**
+     * The Parser instance.
+     *
+     * @var Parser
+     */
+    protected $parser;
+
+    /**
+     * The Evaluator instance.
+     *
+     * @var Evaluator
+     */
+    protected $evaluator;
+
+    /**
+     * The constructor.
+     *
+     * @param Parser|null $parser
+     * @param Evaluator|null $evaluator
+     * @return Builder
+     */
+    public function __construct(Parser $parser = null, Evaluator $evaluator = null)
+    {
+        $this->parser = $parser ?: new Parser;
+
+        $this->evaluator = $evaluator ?: new Evaluator;
+    }
+
+    /**
      * Build a Markdown document from given $content.
      *
      * @param string $content
