5547c3653e9d7747c249c692b56e9bd444ad6b98
Ilya S <public.ilya.s@gmail.com>
2014-05-18 22:17:00 +0700

    inject builder

--- source/Exo/Commands/BuildCommand.php
+++ source/Exo/Commands/BuildCommand.php
@@ -1,7 +1,7 @@
 <?php namespace Exo\Commands;
 
 use Horse\Command, Horse\Input, Horse\Output;
-use Exo\Filesystem;
+use Exo\Filesystem, Exo\Builder;
 
 /**
  * @name build
@@ -18,16 +18,26 @@ class BuildCommand extends Command {
     protected $filesystem;
 
     /**
+     * The Builder instance.
+     *
+     * @var Builder
+     */
+    protected $builder;
+
+    /**
      * The constructor.
      *
      * @param Filesystem|null $filesystem
+     * @param Builder|null $builder
      * @return BuildCommand
      */
-    public function __construct(Filesystem $filesystem = null)
+    public function __construct(Filesystem $filesystem = null, Builder $builder = null)
     {
         parent::__construct();
 
         $this->filesystem = $filesystem ?: new Filesystem;
+
+        $this->builder = $builder ?: new Builder;
     }
 
     /**
